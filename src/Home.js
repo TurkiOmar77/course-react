b76c4ab9be19a19eded75084c90e5eec0d13b584
TurkiOmar77 <turkiomaralsaar@gmail.com>
2025-01-08 12:59:43 +0300

    cours 19

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,35 @@ import BlogsList from "./BlogsList";
 const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
+    const [isPinding ,setIsPinding] = useState(true);
+    const [error,setError] = useState(null);
 
       
       useEffect(()=>{
+        setTimeout(()=>{
         fetch('http://localhost:8000/blogs')
         .then(res=>{
+          console.log(res)
+          if(!res.ok){
+            throw Error('could not fetch the data');
+          }
           return res.json()
         })
         .then(data => {
-          console.log(data)
-          setBlogs(data)
+          setBlogs(data);
+          setIsPinding(false);
+          setError(null);
         })
+        .catch(err =>{
+          setIsPinding(false);
+          setError(err.message);
+        })
+        },1000)
     },[]);
     return ( 
         <div className="home">
+          {error && <div>{error}</div>}
+            {isPinding && <div>Loding ...</div>}
             {blogs && <BlogsList blogs={blogs} title='All Blogs' />}
 
         </div>
